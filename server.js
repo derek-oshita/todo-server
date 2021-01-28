eae1f08e7a8ee31a50c216cbefdc874b55d38a1e
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-01-27 17:16:45 -0800

    Server listening.

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 const express = require('express'); 
 const app = express(); 
 const port = process.env.PORT || 3000; 
+require('dotenv').config(); 
 
 
 
 
 // CONNECTION 
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`)
+}); 
+
