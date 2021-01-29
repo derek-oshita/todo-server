da3e0b381b7d465ba26c28f5aaabe65d9b9a77a7
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-01-28 18:46:03 -0800

    Items Routes completed.

--- routes/items.js
+++ routes/items.js
@@ -3,9 +3,18 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 // ROUTES
-router.get('/', ctrl.items.index); 
 
+// index
+router.get('/', ctrl.items.index); 
+// create
 router.post('/', ctrl.items.create); 
+// show
+router.get('/:id', ctrl.items.show); 
+// update
+router.put('/:id', ctrl.items.update); 
+// destroy
+router.delete('/:id', ctrl.items.destory); 
+
 
 // EXPORTS 
 module.exports = router; 
